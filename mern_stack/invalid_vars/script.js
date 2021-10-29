af50d33e6ce71c7be3c072c360e65ed9f4710686
Aswin Karuvally <aswinbabuk@gmail.com>
2021-10-29 17:26:11 +0530

    added invalid vars sample code

---
+++ mern_stack/invalid_vars/script.js
@@ -0,0 +1,19 @@
+/*
+This is a multiline comment
+It seems JS is friends with the C language
+*/
+
+// Use all the modern JS goodiness
+"use strict";
+
+// Store a person's details
+let 1name = 'Aswin';
+let -age = 40;
+let 1st_occupation = 'Programmer';
+
+// The message to be shown as alert
+let message = 'സന്മനസ്സുള്ളവർക്ക് സമാധാനം';
+let new-message = `My name is ${name} and I am ${age} years old`;
+
+// A simple Hello World statement
+alert(new-message);
