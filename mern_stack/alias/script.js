ea67fec506ac97fc1ba5616478bd06e9f4243409
Aswin Karuvally <aswinbabuk@gmail.com>
2021-10-29 18:34:03 +0530

    added alias examples

---
+++ mern_stack/alias/script.js
@@ -0,0 +1,17 @@
+/*
+This is a multiline comment
+It seems JS is friends with the C language
+*/
+
+// Use all the modern JS goodiness
+"use strict";
+
+// Declaring a constant
+const message = 'ഞാൻ ഒരു കോൺസ്റ്റന്റ് ആകുന്നു.';
+
+// Declaring an alias (this is just a constant with uppercase characters)
+const COLOR_RED = '#F00';
+const COLOR_BLUE = '00F';
+
+// A simple Hello World statement
+alert(message);
