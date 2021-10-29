d633c717f326997ee667759219df03e91f9c3a88
Aswin Karuvally <aswinbabuk@gmail.com>
2021-10-29 18:30:31 +0530

    Added commentS

--- mern_stack/const/script.js
+++ mern_stack/const/script.js
@@ -6,7 +6,10 @@ It seems JS is friends with the C language
 // Use all the modern JS goodiness
 "use strict";
 
+// Declaring a constant
 const message = 'ഞാൻ ഒരു കോൺസ്റ്റന്റ് ആകുന്നു.';
+
+// Trying to change the value of a constant
 message = 'നീ വേരിയബിൾ അല്ലേടാ?';
 
 // A simple Hello World statement
