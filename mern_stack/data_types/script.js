822fb916b50df9e81e1ec3852a441266f11a9a99
Aswin Karuvally <aswinbabuk@gmail.com>
2021-10-29 19:11:40 +0530

    added dynamically typed code

---
+++ mern_stack/data_types/script.js
@@ -0,0 +1,12 @@
+/*
+Proof that JavaScript is a dynamically typed language
+*/
+
+// Use all the modern JS goodiness
+"use strict";
+
+// Let us declare a string
+let message = "This is a message written in English";
+
+// Let's make it a number
+message = 512;
