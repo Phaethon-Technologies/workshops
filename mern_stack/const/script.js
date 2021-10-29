41425f78d1a40081822bfa2b2f442f8ef2c12dcc
Aswin Karuvally <aswinbabuk@gmail.com>
2021-10-29 18:28:56 +0530

    constants in JS

---
+++ mern_stack/const/script.js
@@ -0,0 +1,13 @@
+/*
+This is a multiline comment
+It seems JS is friends with the C language
+*/
+
+// Use all the modern JS goodiness
+"use strict";
+
+const message = 'ഞാൻ ഒരു കോൺസ്റ്റന്റ് ആകുന്നു.';
+message = 'നീ വേരിയബിൾ അല്ലേടാ?';
+
+// A simple Hello World statement
+alert(message);
