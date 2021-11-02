109478fa553199da37b6b919296abebcd045df9d
Aswin Karuvally <aswinbabuk@gmail.com>
2021-11-02 10:10:17 +0530

    Added prompt and confirm code

---
+++ mern_stack/alerts/script.js
@@ -0,0 +1,18 @@
+/*
+Let's try alerting users
+*/
+
+// Use all the modern JS goodiness
+"use strict";
+
+// Get user's age
+let age = prompt("How old are you?", 25);
+
+// A simple alert
+alert(`You are ${age} years old!`);
+
+// Confirm the age
+let isAgeAccurate = confirm(`Are you ${age} years old?`);
+
+// Show the results
+alert(`You selected ${isAgeAccurate}`);
