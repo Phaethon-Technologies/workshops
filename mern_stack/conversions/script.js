c08c365742d17b56a0d8e484aa4a59cba0f3b380
Aswin Karuvally <aswinbabuk@gmail.com>
2021-11-02 11:27:20 +0530

    data conversion examples

--- mern_stack/conversions/script.js
+++ mern_stack/conversions/script.js
@@ -12,4 +12,10 @@ let secondNumber = "6";
 // JS is so advanced, it allows us to multiply strings :D
 alert(`The product of ${firstNumber} and ${secondNumber} is ${firstNumber * secondNumber}`);
 
+// Explicitly converting a string to number
+let myStringNumber = 128;
+let convertedNumber = Number(myStringNumber);
 
+// An undefined variable becomes NaN
+let undefinedVariable;
+alert(`The value of undefinedVariable is ${Number(undefinedVariable)}`);
