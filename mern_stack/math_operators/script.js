1b8c37825588ca22b80c112050a05ea9e54d0864
Aswin Karuvally <aswinbabuk@gmail.com>
2021-11-03 19:43:51 +0530

    added math operators examples

---
+++ mern_stack/math_operators/script.js
@@ -0,0 +1,12 @@
+/*
+Trying out math operators
+*/
+
+// Use all the modern JS goodiness
+"use strict";
+
+// Example for a unary operator
+let negativeNumber = -5;
+
+// Example for binary operator
+let difference = 5 - 3;
