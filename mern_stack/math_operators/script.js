1dd4d4245febbbf0dcbb6989034461e1dbc836f7
Aswin Karuvally <aswinbabuk@gmail.com>
2021-11-03 19:58:54 +0530

    Added concatenation examples

--- mern_stack/math_operators/script.js
+++ mern_stack/math_operators/script.js
@@ -16,3 +16,13 @@ let remainder = 20 % 3;
 
 // Raise 5 to the power 3
 let fivePower = 5 ** 3;
+
+// Stiching two strings together (concatenation)
+let firstString = "Hello ";
+let secondString = "World!";
+let stitchedString = firstString + secondString;
+
+// Stitching a string and a number converts the number to a string
+let anotherString = "The answer to life is ";
+let justANumber = 42;
+let stitch = anotherString + justANumber;
