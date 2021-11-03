d918abc66bc581361ef5682e5c46b04d2493d40d
Aswin Karuvally <aswinbabuk@gmail.com>
2021-11-03 19:45:59 +0530

    Added examples

--- mern_stack/math_operators/script.js
+++ mern_stack/math_operators/script.js
@@ -10,3 +10,9 @@ let negativeNumber = -5;
 
 // Example for binary operator
 let difference = 5 - 3;
+
+// Find the reminder when dividing 20 by 3
+let remainder = 20 % 3;
+
+// Raise 5 to the power 3
+let fivePower = 5 ** 3;
