a2256217faaee88645eff59e1b5955f56f47e16f
Aswin Karuvally <aswinbabuk@gmail.com>
2021-11-05 18:23:29 +0530

    Value of zero quirks

--- mern_stack/comparisons/script.js
+++ mern_stack/comparisons/script.js
@@ -10,3 +10,9 @@ let greaterThanResult = 1 > 2; // This results in false
 
 // Data types are automatically converted to numbers for comparison
 let is3GreaterThan2 = '3' > 2; // Returns true, as 3 gets converted to Number
+
+// As expected, true has the value 1 and false 0
+let isFalseGreaterThanTrue = false > true; // Returns false
+
+// Note that, the value of the String "0" will be > 0 when converted to Boolean
+let valueOfZero = Boolean("0") == 0; // Returns false
