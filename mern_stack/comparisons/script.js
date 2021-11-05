4218408c630c16a04e4374a6da423ddb1db2f3f1
Aswin Karuvally <aswinbabuk@gmail.com>
2021-11-05 18:17:13 +0530

    Automatic conversion of data types to numbers

--- mern_stack/comparisons/script.js
+++ mern_stack/comparisons/script.js
@@ -8,4 +8,5 @@ Doing comparisons in JS
 // Comparisons result in booleans
 let greaterThanResult = 1 > 2; // This results in false
 
-
+// Data types are automatically converted to numbers for comparison
+let is3GreaterThan2 = '3' > 2; // Returns true, as 3 gets converted to Number
