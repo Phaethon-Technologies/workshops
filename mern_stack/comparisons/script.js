260653ebab542cf757900332b08d814bc5c9f245
Aswin Karuvally <aswinbabuk@gmail.com>
2021-11-05 18:12:01 +0530

    Comparison examples

---
+++ mern_stack/comparisons/script.js
@@ -0,0 +1,11 @@
+/*
+Doing comparisons in JS
+*/
+
+// Use all the modern JS goodiness
+"use strict";
+
+// Comparisons result in booleans
+let greaterThanResult = 1 > 2; // This results in false
+
+
