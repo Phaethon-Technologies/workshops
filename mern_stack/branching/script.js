d50f5351b909eae4a63f34dadcfa2e16c2fc6074
Aswin Karuvally <aswinbabuk@gmail.com>
2021-11-06 16:10:29 +0530

    Branching examples

---
+++ mern_stack/branching/script.js
@@ -0,0 +1,16 @@
+/*
+Let's do branching!
+*/
+
+// Use all the modern JS goodiness
+"use strict";
+
+// Single line if
+let isLifeGood = true;
+if (isLifeGood == true) alert("Life's good!");
+
+// Multiline body for if
+if (isLifeGood == true) {
+    alert("Life's good!");
+    alert("Do you know that the above statement is LG's trademark?");
+}
