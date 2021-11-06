969933ac71093b35de77151ab4fdd74db6da521f
Aswin Karuvally <aswinbabuk@gmail.com>
2021-11-06 17:50:37 +0530

    examples of OR returning values without conversion

--- mern_stack/logical_operators/script.js
+++ mern_stack/logical_operators/script.js
@@ -13,3 +13,10 @@ let age = 30;
 if (age < 18 || age > 60) {
     alert(`You are not eligible as you are ${age} years old`);
 }
+
+// OR would return the first truthy value without conversion
+let firstName = "";
+let lastName = "Kurian";
+let givenName = firstName || lastName || null; // Stores Kurian as givenName
+
+
