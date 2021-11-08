8f3a740d92f2fc9fc567f8303343faa0705c5ec8
Aswin Karuvally <aswinbabuk@gmail.com>
2021-11-08 11:07:19 +0530

    Functions examples

---
+++ mern_stack/functions/script.js
@@ -0,0 +1,14 @@
+/*
+Funcitons in JS
+*/
+
+// Use all the modern JS goodiness
+"use strict";
+
+// Function declaration
+function myFirstFunction() {
+    alert("This works!");
+}
+
+// Function call
+myFirstFunction();
