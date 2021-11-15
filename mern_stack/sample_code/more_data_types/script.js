62d3622b8732c75ba72bc4478274905d30c2284e
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2021-11-15 20:40:06 +0530

    Added array methods

--- mern_stack/sample_code/more_data_types/script.js
+++ mern_stack/sample_code/more_data_types/script.js
@@ -14,4 +14,14 @@ console.log(`My favourite fruit is ${fruits[1]}`);
 // Modify an existing element
 fruits[3] = "Mango";
 
+// Add an element to the end of the array
+fruits.push("Jackfruit");
 
+// Remove last element from array
+console.log(`Removing ${fruits.pop()}`);
+
+// Add a new element to the beginning of the array
+fruits.unshift("Blackberry");
+
+// Remove the first element in the array (shift the array forward by one item)
+console.log(`Removing ${fruits.shift()}`);
