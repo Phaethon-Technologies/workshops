658ad85446968836abc367be7230f32d5cd26825
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2021-11-15 19:00:06 +0530

    Added Symbol.for() and Symbol.keyFor() methods

--- mern_stack/sample_code/symbols/script.js
+++ mern_stack/sample_code/symbols/script.js
@@ -14,3 +14,13 @@ let userDetails = {
     "name": "Jobs, Steve",
     "employer": "Apple",
 }
+
+// Create a global symbol
+let deptID = Symbol.for("deptID");
+
+// Add the symbol to userDetails object
+// The call accesses global registry and finding Symbol for "deptID" already exists, returns it
+userDetails[Symbol.for("deptID")] = 1080;
+
+// Reverse call to find out the title of a symbol
+console.log(`The title of symbol deptID is ${Symbol.keyFor(deptID)}`);
