26661fbb5534cb80d37337bab22868c446fcc7ed
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2021-11-15 20:26:54 +0530

    Using array

---
+++ mern_stack/sample_code/more_data_types/script.js
@@ -0,0 +1,17 @@
+/*
+Exploring more data types
+*/
+
+// Use all the modern JS goodiness
+"use strict";
+
+// Declaring an array
+let fruits = ["Apple", "Orange", "gooseberry", "Strawberry",]
+
+// Access the second element from the array
+console.log(`My favourite fruit is ${fruits[1]}`);
+
+// Modify an existing element
+fruits[3] = "Mango";
+
+
