062c6932a6f91666783f3e97cb2bfc3731c4ff72
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2021-11-16 18:49:11 +0530

    Exercise for naming variables

---
+++ mern_stack/exercises/variables.js
@@ -0,0 +1,3 @@
+// Create a variable with the name of our planet. How would you name such a variable?
+// Create a variable to store the name of a current visitor to a website. How would you name that variable?
+// Create a variable to store the name of an automobile manufacturer, how would you name it?
