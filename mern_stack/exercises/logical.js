06794d8d8c5f91555da5534cd14b6b3a65cb5d28
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2021-11-16 19:31:18 +0530

    Added logical operator exercises

---
+++ mern_stack/exercises/logical.js
@@ -0,0 +1,14 @@
+// What will be the result of the following code
+alert( null || 2 || undefined );
+
+// Will alert(3) execute?
+alert( alert(1) || 2 || alert(3) );
+
+// What will be the output of the following code
+alert( 1 && null && 2 );
+
+// What will be the results of the following code
+alert( alert(1) && alert(2) );
+
+// Guess the output of the following code
+alert( null || 2 && 3 || 4 );
