8a644d9c5cdde7816aab86f8a388e5bf3a704838
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2021-11-16 19:06:46 +0530

    Added conditions

---
+++ mern_stack/exercises/conditions.js
@@ -0,0 +1,30 @@
+// Will the alert work?
+if ("0") {
+    alert( 'Hello' );
+}
+
+// Using if..else, write the code which gets a number via prompt and then shows in alert:
+// Positive, if the value is greater than zero,
+// Negative, if less than zero,
+// Zero, if equals zero,
+// InvalidValue, if the input is not a number
+
+// Rewrite the following code using conditional operator
+let result;
+if (a + b < 4) {
+  result = 'Below';
+} else {
+  result = 'Over';
+}
+
+// Rewrite the following if else construct using conditional operator
+let message;
+if (login == 'Employee') {
+  message = 'Hello';
+} else if (login == 'Director') {
+  message = 'Greetings';
+} else if (login == '') {
+  message = 'No login';
+} else {
+  message = '';
+}
