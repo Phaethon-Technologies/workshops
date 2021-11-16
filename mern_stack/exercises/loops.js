bfcd773f5b6d23826c2bbe7f415e3491298a9548
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2021-11-16 19:51:19 +0530

    Loop exercises

---
+++ mern_stack/exercises/loops.js
@@ -0,0 +1,9 @@
+// Modify the following code to use while loop
+for (let i = 0; i < 3; i++) {
+    alert( `number ${i}!` );
+}
+
+// Which values will be output by the following loop
+for (let i = 0; i < 5; i++) alert( i );
+
+// Write code to calculate all prime numbers upto a user specified limit
