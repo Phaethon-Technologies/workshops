74c86f76d095706e32a0ce60f7ea29ebaed49fb9
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2021-11-16 18:52:37 +0530

    naming constants

--- mern_stack/exercises/variables.js
+++ mern_stack/exercises/variables.js
@@ -1,3 +1,11 @@
+// Task 1
 // Create a variable with the name of our planet. How would you name such a variable?
 // Create a variable to store the name of a current visitor to a website. How would you name that variable?
 // Create a variable to store the name of an automobile manufacturer, how would you name it?
+
+// Task 2
+// In the below code, would it be okay to use uppercase for birthday or age?
+const birthday = '25-12-1970';
+const age = calculateAge(birthday); // custom function
+
+
