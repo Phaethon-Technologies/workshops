7fde397f2af5e4e1d100ec5ca2e3fea7a31f98fb
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2021-11-16 15:08:34 +0530

    Using date objects

---
+++ mern_stack/sample_code/date/script.js
@@ -0,0 +1,17 @@
+/*
+Date and time in JavaScript
+*/
+
+// Use all the modern JS goodiness
+"use strict";
+
+// Get the current date and time
+let currentTime = new Date();
+
+// Create a Date object with time equal to 1000 msecs after UNIX epoch
+let unixEpochTime = new Date(1000);
+
+// Set 2006-12-25 as the date
+let christmas2006 = new Date("2006-12-25");
+
+
