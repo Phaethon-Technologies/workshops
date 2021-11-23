63889f3fd523909c6ffeed3db5a30de92f80f812
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2021-11-23 10:38:29 +0530

    extending class from base class

---
+++ mern_stack/sample_code/overriding_methods/script.js
@@ -0,0 +1,31 @@
+/*
+Extending Classes
+*/
+
+// Use all the modern JS goodiness
+"use strict";
+
+// Class for normal employee
+class Employee {
+    constructor(name, department, baseSalary) {
+        this.name = name;
+        this.department = department;
+        this.baseSalary = baseSalary;
+    }
+
+    // Calculate the total salary of the employee
+    getSalary() {
+        let hra = this.baseSalary * (10/100);
+        let travelAllowance = this.baseSalary * (15/100);
+        let tuitionAllowance = this.baseSalary * (20/100);
+        return this.baseSalary + hra + travelAllowance + tuitionAllowance;
+    }
+}
+
+// Class for interns
+class Intern extends Employee {
+}
+
+// Create an intern object
+let jiju = new Intern("Jiju Varghese", "Finance", 25000);
+
