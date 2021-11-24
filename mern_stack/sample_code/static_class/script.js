15468c362d3eb053865f9b2a45c900107f5a4765
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2021-11-24 20:35:26 +0530

    Static methods example

--- mern_stack/sample_code/static_class/script.js
+++ mern_stack/sample_code/static_class/script.js
@@ -7,6 +7,12 @@ Static methods in classes
 
 // Class with static method 
 class Keyboard {
+    constructor(identifier) {
+        this.identifier = identifier;
+    }
+    static getName() {
+        return this.identifier;
+    }
     static checkThis() {
         console.log(this === Keyboard);
     }
@@ -14,8 +20,25 @@ class Keyboard {
 
 // Class without static method
 class Mouse {
+    constructor(identifier) {
+        this.identifier = identifier;
+    }
+    getName() {
+        return this.identifier;
+    }
     checkThis() {
         console.log(this === Mouse);
     }
 }
+
+// Create objects
+let logitech = new Mouse("Wireless");
+let microsoft = new Keyboard("PS/2");
+
+// This results in an error
+// microsoft.getName();
+
+// But this works (static methods can only be invoked using class identifier)
+// One important thing to note here is that static methods cannot access object values
+Keyboard.getName();
 
