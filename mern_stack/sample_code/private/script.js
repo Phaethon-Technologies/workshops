c514b4358bbd1bfdb2ac428c5a59c77f731b9c56
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2021-11-25 11:58:28 +0530

    Added class with protected members

---
+++ mern_stack/sample_code/private/script.js
@@ -0,0 +1,28 @@
+/*
+Protected and private properties
+*/
+
+// Use all the modern JS goodiness
+"use strict";
+
+// Class with a private number, green, blue values 
+class Interrupt{
+    set number(value) {
+        this._number = this._cleanedValue(value);
+    }
+    get number() {
+        return this._number;
+    }
+
+    // Clean values
+    _cleanedValue(value) {
+        // Fix out of range values 
+        if (value > 255) {
+            return 255;
+        } elif (value < 0) {
+            return 0;
+        }
+        return value;
+    }
+}
+
