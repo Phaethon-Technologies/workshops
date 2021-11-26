19624ee73ceedbe6c5eced015fd0036c8a35a13c
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2021-11-26 18:07:37 +0530

    Added BOM object values

--- mern_stack/sample_code/browser_env/script.js
+++ mern_stack/sample_code/browser_env/script.js
@@ -10,3 +10,9 @@ document.body.style.background = "black";
 // Change the background colour back to white
 setTimeout(() => document.body.style.background = "", 1000);
 
+// Get the userAgent
+alert(`Your userAgent is -> ${navigator.userAgent}`);
+
+// Get the platform
+alert(`You are running ${navigator.platform}`);
+
