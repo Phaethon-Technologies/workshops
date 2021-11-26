5a778d296de51f807a7793ebb1b6d07a9de196a5
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2021-11-26 17:05:58 +0530

    Code to switch background colours

---
+++ mern_stack/sample_code/browser_env/script.js
@@ -0,0 +1,12 @@
+/*
+Using JavaScript in browser environment
+*/
+
+// Use all the modern JS goodiness
+"use strict";
+
+// Set background colour as black
+document.body.style.background = "black";
+// Change the background colour back to white
+setTimeout(() => document.body.style.background = "", 1000);
+
