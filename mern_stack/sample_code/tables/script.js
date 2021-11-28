39594e0a90cbdde9d70b6936495b9b51ea9aa4bb
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2021-11-28 22:53:32 +0530

    Methods for manipulating table

--- mern_stack/sample_code/tables/script.js
+++ mern_stack/sample_code/tables/script.js
@@ -5,3 +5,17 @@ Manipulating tables using JavaScript and DOM
 // Use all the modern JS goodiness
 "use strict";
 
+// Accessing the table with the id myTable 
+console.log("Here are myTable's rows");
+// First method
+console.log(myTable.rows);
+// Alternate way of accessing rows
+console.log(myTable.tBodies[0].rows);
+
+// Change the background colour of first row to Brown 
+myTable.rows[0].bgColor = "Brown";
+
+// Access the zeroth row's zeroth element
+console.log(`Zeroth row's zeroth element is`);
+console.log(myTable.rows[0].cells[1]);
+
