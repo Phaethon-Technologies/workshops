0aa59db3d74ff944b83d630cd3589e341bfa8433
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2021-12-01 11:20:57 +0530

    Added then function to handle promises

--- mern_stack/sample_code/promise/script.js
+++ mern_stack/sample_code/promise/script.js
@@ -5,8 +5,26 @@ Promises in JS
 // Use all the modern JS goodiness
 "use strict";
 
-// Our first promise
-let promise = new Promise(function(resolve, reject) {
-    setTimeout(() => resolve("done"), 1000); 
+// A promise which completes succesfuly 
+let fulfilledPromise = new Promise(function(resolve, reject) {
+    let currentTime = Date();
+    setTimeout(() => resolve(currentTime), 1000); // "completed" can be any value 
 });
+
+// Handle fulfilledPromise 
+fulfilledPromise.then(
+    result => alert(result),
+    error => alert(error)
+)
+
+// A promise which fails
+let failedPromise = new Promise(function(resolve, reject) {
+    setTimeout(() => reject(new Error("This promise failed")), 3000);
+});
+
+// Handle failedPromise
+failedPromise.then(
+    result => alert(result),
+    error => alert(error)
+)
 
