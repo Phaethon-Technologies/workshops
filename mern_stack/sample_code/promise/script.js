7f263ba7c2c2a725aa6660a2a19a2d1d76e093a8
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2021-12-01 16:49:02 +0530

    Added finally clause

--- mern_stack/sample_code/promise/script.js
+++ mern_stack/sample_code/promise/script.js
@@ -23,17 +23,14 @@ let failedPromise = new Promise(function(resolve, reject) {
 });
 
 // Handle failedPromise
-failedPromise.then(
-    result => alert(result),
-    error => alert(error)
-)
+failedPromise.catch(error => alert(error));
 
 // Below code fails, as we end up calling the alert function,
 // without passing it to the Promise.then() method
 // failedPromise.then(alert(value), alert(value));
 
 // A simple Christmas checker
-let anotherPromise = new Promise(function(resolve, reject) {
+let xmasPromise = new Promise(function(resolve, reject) {
     // Get date and month
     let dateObj = new Date();
     if (dateObj.getDate() == 25 && dateObj.getMonth() == 11) {
@@ -42,10 +39,13 @@ let anotherPromise = new Promise(function(resolve, reject) {
     reject("It's not Santa's time yet");
 });
 
-// Let's handle anotherPromise 
-anotherPromise.then(
+// Let's handle xmasPromise 
+// The finally clause executes no matter what happens to a promise
+xmasPromise.then(
     successMessage => promiseLogger(successMessage),
     failureMessage => promiseLogger(failureMessage)
+).finally(
+    () => promiseLogger("End of the road!")
 );
 
 // Log promise results to the console
