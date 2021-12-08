813397a8dcd1c4401ee3bf60df3e0c4427acc104
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2021-12-08 19:03:46 +0530

    Project only needed info after find operation

---
+++ mern_stack/sample_code/nodejs/mongo/findManyProjection.js
@@ -0,0 +1,46 @@
+// Import required libraires
+const { MongoClient } = require("mongodb");
+
+// Connection URI
+const uri = "mongodb://127.0.0.1:27017";
+
+// Create new MongoClient
+const client = new MongoClient(uri);
+
+// Set the DB and collection
+const database = client.db("workshop");
+const collection = database.collection("students");
+
+// The query to be passed to DB
+const query = { "semester": "S6" };
+
+// Options to use
+const options = {
+    sort: { "roll": 1 },
+    projection: { _id: 0 },
+};
+
+async function mongoConnect(collection, query, options) {
+    try {
+        // Connect to DB
+        await client.connect();
+
+        // Setup cursor to find all matching documents
+        const cursor = await collection.find(query, options);
+
+        // Alert user if no documents were found
+        if ((await cursor.count()) == 0) {
+            console.log("No documents found!");
+        }
+
+        // Print each document found
+        await cursor.forEach(console.dir);
+    }
+    finally {
+        await client.close();
+    }
+}
+
+// Connect to MongoDB and do stuff
+mongoConnect(collection, query, options).catch(console.dir);
+
