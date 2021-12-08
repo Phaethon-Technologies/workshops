b509b47c83ace91356e87854dedd82f965688b10
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2021-12-08 16:15:29 +0530

    MongoDB connection example

--- mern_stack/sample_code/nodejs/mongo/app.js
+++ mern_stack/sample_code/nodejs/mongo/app.js
@@ -1,1 +1,21 @@
-const mongo = require("mongodb");
+// Import required libraires
+const { MongoClient } = require("mongodb");
+
+// Connection URI
+const uri = "mongodb://127.0.0.1:27017";
+
+// Create new MongoClient
+const client = new MongoClient(uri);
+
+async function mongoConnect() {
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB server");
+    }
+    finally {
+        await client.close();
+    }
+}
+
+mongoConnect().catch(console.dir);
+
